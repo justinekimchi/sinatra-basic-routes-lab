f3f3a61351ddd6d22ee12c20848065c12bb88975
justinekimchi <justine.kim.manaloto@gmail.com>
2020-10-26 20:23:53 -0500

    commit tree 32760ba66c5e05860634368e6fc1aab14f55df5f

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Justine"
+  end
+
+  get '/hometown' do
+    "My hometown is San Antonio"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Remember Me'"
+  end
+
+
+
 end
